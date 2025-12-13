271d59dc105248bc8fb0d4909c839c3e7f8814a5
miwa <miwa@miwa-MacBook-Air.local>
2025-12-13 16:22:23 +0300

    feat: add environment configuration and database setup for PostgreSQL

--- ip-lookup-backend/src/app.module.ts
+++ ip-lookup-backend/src/app.module.ts
@@ -16,7 +16,7 @@ import {IpInfoModule}from './ip-info/ip-info.module';
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true, 
       synchronize: true,  }),
