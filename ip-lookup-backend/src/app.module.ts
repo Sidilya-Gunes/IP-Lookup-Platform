48f9b8d8540e4e1cc1c3c201c48db681524f1d14
miwa <miwa@miwa-MacBook-Air.local>
2025-12-16 14:44:38 +0300

    feat: add throttling and request logging

--- ip-lookup-backend/src/app.module.ts
+++ ip-lookup-backend/src/app.module.ts
@@ -1,16 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module , NestModule , MiddlewareConsumer} from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import {ConfigModule}from '@nestjs/config';
+import {ConfigModule, ConfigService}from '@nestjs/config';
 import {TypeOrmModule}from '@nestjs/typeorm';
 
 import {IpInfoModule}from './ip-info/ip-info.module';
 
 
+import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
+
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal:true}),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject:[ConfigService],
+      useFactory: (config : ConfigService) => ([{
+        ttl: 60000, // 1 minute
+        limit : 20 , // 20 requests per minute
+      }]) ,
+     }) ,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -19,10 +33,20 @@ import {IpInfoModule}from './ip-info/ip-info.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true, 
-      synchronize: true,  }),
+      synchronize: true,  
+    }),
     IpInfoModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer : MiddlewareConsumer){
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
