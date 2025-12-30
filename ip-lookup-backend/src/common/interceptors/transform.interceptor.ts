2a7692405a453c730cf90c28a8ca850a1d766637
miwa <sisyaarmy2121@gmail.com>
2025-12-30 13:35:45 +0300

    feat: add Interceptor

---
+++ ip-lookup-backend/src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { NestInterceptor , ExecutionContext, CallHandler } from "@nestjs/common/interfaces";
+import e from "express";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+export interface Response<T> {
+    success: boolean;
+    data: T;
+    timestamp: string;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>>{
+        return next.handle().pipe(
+            map((data)=>({
+                success: true,
+                data,
+                timestamp: new Date().toISOString(),
+            })),
+        )
+    }
+}
--- ip-lookup-backend/src/main.ts
+++ ip-lookup-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
