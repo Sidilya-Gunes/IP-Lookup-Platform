78852e7b6f3c1d9926e876d6406b8729ef427c60
miwa <sisyaarmy2121@gmail.com>
2025-12-30 18:34:41 +0300

    chore: remove TransformInterceptor import and usage from main.ts

--- ip-lookup-backend/src/main.ts
+++ ip-lookup-backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +15,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
 
-  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(process.env.PORT ?? 3002);
 }
